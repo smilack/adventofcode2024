829ef76e027ef0a9624409741125aa493189fb08
Tom Smilack <tom@smilack.net>
2024-12-03 12:47:51 -0600

    Day 1 part 1 tests working but wrong answer

--- src/One.purs
+++ src/One.purs
@@ -1,25 +1,77 @@
 module AdventOfCode.Twenty24.One
-  ( main
+  ( distances
+  , main
+  , pairUp
+  , parse
+  , solve1
+  , sort
   ) where
 
 import Prelude
-import AdventOfCode.Twenty24.Util
 
+import AdventOfCode.Twenty24.One.Pair (Pair(..))
+import Data.Either (Either, fromRight)
+import Data.List (List(..))
+import Data.List as List
+import Data.List.Lazy as Lazy
+import Data.List.ZipList (ZipList(..))
+import Data.Newtype (unwrap)
+import Data.Ord (abs)
+import Data.Traversable (sequence, sum)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
-import Parsing (Parser)
+import Parsing (Parser, runParser)
+import Parsing.Combinators (many)
+import Parsing.String.Basic (intDecimal, skipSpaces)
 
 main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "./input/1"
   liftEffect do
     log "Part 1:"
-    -- log ""
-    -- logShow $ solve1 input
-    log "Part2:"
-    -- log ""
-    -- logShow $ solve2 input
+    logShow $ solve1 input
+    log "Part 2:"
+
+-- log ""
+-- logShow $ solve2 input
+
+solve1 :: String -> Int
+solve1 = sum <<< distances <<< pairUp <<< sort <<< parse
+
+sort :: Pair (List Int) -> Pair (List Int)
+sort = map List.sort
+
+pairUp :: forall a. Pair (List a) -> List (Pair a)
+pairUp =
+  List.fromFoldable
+    <<< unwrap -- Lazy.List (Pair a)
+    <<< sequence -- :: ZipList (Pair a))
+    <<< map ZipList -- :: Pair (ZipList Int)
+    <<< map Lazy.fromFoldable -- :: Pair (Lazy.List Int)
+
+distances :: forall a f. Ord a => Ring a => Functor f => f (Pair a) -> f a
+distances = map distance
+  where
+  distance (Pair { a, b }) = abs $ a - b
+
+parse :: String -> Pair (List Int)
+parse s = toPairedLists $ runParser s readLines
+  where
+  toPairedLists :: forall a. Either a (List (Pair Int)) -> Pair (List Int)
+  toPairedLists = sequence <<< fromRight Nil
+
+readLines :: Parser String (List (Pair Int))
+readLines = many readLine
+
+readLine :: Parser String (Pair Int)
+readLine = do
+  skipSpaces
+  a <- intDecimal
+  skipSpaces
+  b <- intDecimal
+  pure $ Pair { a, b }
+
--- test/One.purs
+++ test/One.purs
@@ -3,24 +3,82 @@ module Test.AdventOfCode.Twenty24.One
   ) where
 
 import Prelude
-import AdventOfCode.Twenty24.One
-import AdventOfCode.Twenty24.Util
-import Data.String (split)
-import Data.String.Pattern (Pattern(..))
+
+import AdventOfCode.Twenty24.One (distances, pairUp, parse, solve1, sort)
+import AdventOfCode.Twenty24.One.Pair (Pair(..))
+import Data.List.Types (List(..), (:))
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Test.QuickCheck ((===), Result)
-import Test.Spec (Spec, pending, describe, it)
+import Test.Spec (describe, it, pending)
 import Test.Spec.Assertions (shouldEqual)
-import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (runSpec)
+import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [ consoleReporter ] do
+main = runSpecAndExitProcess [ consoleReporter ] do
   describe "Day One" do
     describe "Part 1" do
-      pending "parse input"
-      pending "other stuff"
+      it "Parses input" do
+        parse parseTest.input `shouldEqual` parseTest.output
+        parse inputTest.input `shouldEqual` inputTest.output
+      it "Sorts lists" do
+        (sort sortTest.input) `shouldEqual` sortTest.output
+      it "Inverts list/pair" do
+        (pairUp sequenceTest.input) `shouldEqual` sequenceTest.output
+      it "Calculates distances" do
+        (distances distanceTest.input) `shouldEqual` distanceTest.output
+      it "Solves part 1" do
+        solve1 puzzle1Test.input `shouldEqual` puzzle1Test.output
     describe "Part 2" do
       pending "more stuff"
+
+parseTest :: { input :: String, output :: Pair (List Int) }
+parseTest =
+  { input: "3   4\n4   3"
+  , output: Pair { a: 3 : 4 : Nil, b: 4 : 3 : Nil }
+  }
+
+inputTest :: { input :: String, output :: Pair (List Int) }
+inputTest =
+  { input:
+      """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+  , output: Pair
+      { a: 3 : 4 : 2 : 1 : 3 : 3 : Nil
+      , b: 4 : 3 : 5 : 3 : 9 : 3 : Nil
+      }
+  }
+
+sortTest :: { input :: Pair (List Int), output :: Pair (List Int) }
+sortTest =
+  { input: inputTest.output
+  , output: Pair
+      { a: 1 : 2 : 3 : 3 : 3 : 4 : Nil
+      , b: 3 : 3 : 3 : 4 : 5 : 9 : Nil
+      }
+  }
+
+sequenceTest :: { input :: Pair (List Int), output :: List (Pair Int) }
+sequenceTest =
+  { input: sortTest.output
+  , output:
+      Pair { a: 1, b: 3 }
+        : Pair { a: 2, b: 3 }
+        : Pair { a: 3, b: 3 }
+        : Pair { a: 3, b: 4 }
+        : Pair { a: 3, b: 5 }
+        : Pair { a: 4, b: 9 }
+        : Nil
+  }
+
+distanceTest :: { input :: List (Pair Int), output :: List Int }
+distanceTest =
+  { input: sequenceTest.output
+  , output: 2 : 1 : 0 : 1 : 2 : 5 : Nil
+  }
+
+puzzle1Test :: { input :: String, output :: Int }
+puzzle1Test = inputTest { output = 11 }
