770c5a480dbdd4d731557400c9f2c2350f1d07c9
Tom Smilack <tom@smilack.net>
2024-12-03 17:51:31 -0600

    Solve day 2 part 1

--- src/Two.purs
+++ src/Two.purs
@@ -1,25 +1,94 @@
 module AdventOfCode.Twenty24.Two
-  ( main
+  ( Safety(..)
+  , check
+  , countSafe
+  , main
+  , parse
+  , solve1
   ) where
 
-import Prelude
 import AdventOfCode.Twenty24.Util
+import Prelude
 
+import Data.Either (fromRight)
+import Data.Foldable (class Foldable)
+import Data.Generic.Rep (class Generic)
+import Data.List (List(..), (:))
+import Data.List as List
+import Data.List.NonEmpty (fromList)
+import Data.List.NonEmpty as NE
+import Data.List.Types (NonEmptyList)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Ord (abs)
+import Data.Show.Generic (genericShow)
+import Data.Traversable (traverse)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
-import Parsing (Parser)
+import Parsing (Parser, runParser)
+import Parsing.Combinators (endBy, many, many1, sepBy, sepBy1, sepEndBy, skipMany, skipMany1, (<|>))
+import Parsing.String (string)
+import Parsing.String.Basic (intDecimal, whiteSpace)
+import PointFree ((<..))
 
 main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "./input/2"
   liftEffect do
     log "Part 1:"
-    -- log ""
-    -- logShow $ solve1 input
+    logShow $ solve1 input
     log "Part2:"
-    -- log ""
-    -- logShow $ solve2 input
+
+-- log ""
+-- logShow $ solve2 input
+
+data Safety = Safe | Unsafe
+
+derive instance Eq Safety
+derive instance Ord Safety
+derive instance Generic Safety _
+instance Show Safety where
+  show = genericShow
+
+solve1 :: String -> Int
+solve1 = countSafe <<< map check <<< parse
+
+countSafe :: List Safety -> Int
+countSafe = lookupWithDefault 0 Safe <<< tally
+
+check :: NonEmptyList Int -> Safety
+check nel = go first tail
+  where
+  { head: first, tail } = NE.uncons nel
+
+  second = case List.uncons tail of
+    Nothing -> first
+    Just rest -> rest.head
+
+  ord = compare first second
+
+  diff = abs <.. (-)
+
+  go :: Int -> List Int -> Safety
+  go _ Nil = Safe
+  go prev (next : rest) = case compare prev next of
+    EQ -> Unsafe
+    ord'
+      | ord /= ord' -> Unsafe
+      | diff prev next > 3 -> Unsafe
+      | otherwise -> go next rest
+
+parse :: String -> List (NonEmptyList Int)
+parse s = fromRight Nil $ runParser s readReports
+
+readReports :: Parser String (List (NonEmptyList Int))
+readReports = readReport `sepEndBy` eol
+
+readReport :: Parser String (NonEmptyList Int)
+readReport = intDecimal `sepBy1` string " "
+
+eol :: Parser String String
+eol = string "\n" <|> string "\r\n"
--- test/Two.purs
+++ test/Two.purs
@@ -2,9 +2,13 @@ module Test.AdventOfCode.Twenty24.Two
   ( main
   ) where
 
-import Prelude
-import AdventOfCode.Twenty24.Two
 import AdventOfCode.Twenty24.Util
+import Prelude
+
+import AdventOfCode.Twenty24.Two (Safety(..), check, countSafe, parse, solve1)
+import Data.List (List(..), fromFoldable, (:))
+import Data.List.NonEmpty (cons')
+import Data.List.Types (NonEmptyList)
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
@@ -14,13 +18,52 @@ import Test.Spec (Spec, pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (runSpec)
+import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [ consoleReporter ] do
+main = runSpecAndExitProcess [ consoleReporter ] do
   describe "Day Two" do
     describe "Part 1" do
-      pending "parse input"
-      pending "other stuff"
+      it "Parses input 1" do
+        parse input `shouldEqual` reports
+      it "Parses input 2" do
+        parse input2 `shouldEqual` reports
+      it "Parses input 3" do
+        parse input3 `shouldEqual` reports
+      it "Checks reports" do
+        map check reports `shouldEqual` safety
+      it "Counts safe reports" do
+        countSafe safety `shouldEqual` 2
+      it "Solves part 1" do
+        solve1 input `shouldEqual` 2
     describe "Part 2" do
       pending "more stuff"
+
+input :: String
+input = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"
+
+input2 :: String
+input2 = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9\n"
+
+input3 :: String
+input3 = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9\n\n"
+
+reports :: List (NonEmptyList Int)
+reports = fromFoldable
+  [ cons' 7 $ 6 : 4 : 2 : 1 : Nil
+  , cons' 1 $ 2 : 7 : 8 : 9 : Nil
+  , cons' 9 $ 7 : 6 : 2 : 1 : Nil
+  , cons' 1 $ 3 : 2 : 4 : 5 : Nil
+  , cons' 8 $ 6 : 4 : 4 : 1 : Nil
+  , cons' 1 $ 3 : 6 : 7 : 9 : Nil
+  ]
+
+safety :: List Safety
+safety = fromFoldable
+  [ Safe
+  , Unsafe
+  , Unsafe
+  , Unsafe
+  , Unsafe
+  , Safe
+  ]
