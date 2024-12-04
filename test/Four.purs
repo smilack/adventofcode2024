5b8cbb6209ea204c42a791cd475ccdec2d94179c
Tom Smilack <tom@smilack.net>
2024-12-04 11:45:39 -0600

    Add another parsing test

--- test/Four.purs
+++ test/Four.purs
@@ -25,6 +25,7 @@ main = runSpecAndExitProcess [ consoleReporter ] do
     describe "Part 1" do
       it "parses input" do
         parse parseTest.input `shouldEqual` parseTest.output
+        parse parseTest2.input `shouldEqual` parseTest2.output
       it "Solves part 1" do
         solve1 input `shouldEqual` solution1
     describe "Part 2" do
@@ -63,3 +64,38 @@ input =
 
 solution1 :: Int
 solution1 = 18
+
+parseTest2 :: { input :: String, output :: Map Point Char }
+parseTest2 =
+  { input: "MMMS\nMSAM\nAMXS\nMSAM"
+  , output: Map.fromFoldable
+      [ (Point { x: 0, y: 0 }) /\ 'M'
+      , (Point { x: 1, y: 0 }) /\ 'M'
+      , (Point { x: 2, y: 0 }) /\ 'M'
+      , (Point { x: 3, y: 0 }) /\ 'S'
+      , (Point { x: 0, y: 1 }) /\ 'M'
+      , (Point { x: 1, y: 1 }) /\ 'S'
+      , (Point { x: 2, y: 1 }) /\ 'A'
+      , (Point { x: 3, y: 1 }) /\ 'M'
+      , (Point { x: 0, y: 2 }) /\ 'A'
+      , (Point { x: 1, y: 2 }) /\ 'M'
+      , (Point { x: 2, y: 2 }) /\ 'X'
+      , (Point { x: 3, y: 2 }) /\ 'S'
+      , (Point { x: 0, y: 3 }) /\ 'M'
+      , (Point { x: 1, y: 3 }) /\ 'S'
+      , (Point { x: 2, y: 3 }) /\ 'A'
+      , (Point { x: 3, y: 3 }) /\ 'M'
+      ]
+  }
+
+miniSolution2 :: { input :: Map Point Char, output :: Int }
+miniSolution2 =
+  { input: parseTest2.output
+  , output: 1
+  }
+
+solution2 :: { input :: Map Point Char, output :: Int }
+solution2 =
+  { input: parse input
+  , output: 9
+  }
