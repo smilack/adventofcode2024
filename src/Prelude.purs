65c54596df8b13e3b04119454083722a55866184
Tom Smilack <tom@smilack.net>
2024-12-05 15:02:20 -0600

    Add a bunch of useful stuff to AoC Prelude

--- src/Prelude.purs
+++ src/Prelude.purs
@@ -17,3 +17,36 @@ import Prelude
   , (<$>), (<*), (<*>), (<<<), (<=), (<=<), (<>), (<@>), (=<<), (==), (>), (>=)
   , (>=>), (>>=), (>>>), (||)
   ) as Exports
+
+import Data.Either (Either, fromRight) as Exports
+
+import Data.Generic.Rep (class Generic) as Exports
+import Data.Show.Generic (genericShow) as Exports
+
+import Data.List (List(..), (:)) as Exports
+import Data.List.Types (NonEmptyList) as Exports
+
+import Data.Maybe (Maybe(..)) as Exports
+
+import Data.Map (Map) as Exports
+
+import Data.Newtype
+  (wrap, unwrap, class Newtype, un, modify, ala, over, under) as Exports
+
+import Data.Tuple (Tuple(..), uncurry) as Exports
+import Data.Tuple.Nested (type (/\), (/\)) as Exports
+
+import Data.Traversable
+  (class Foldable, class Traversable, traverse, sequence, for, foldl, foldr
+  , foldMap, fold
+  ) as Exports
+
+import Effect (Effect) as Exports
+import Effect.Aff (launchAff_) as Exports
+import Effect.Class (liftEffect) as Exports
+import Effect.Console (log, logShow) as Exports
+
+import Parsing (Parser, runParser) as Exports
+import Parsing.Combinators (many, try) as Exports
+
+import PointFree ((<..)) as Exports
