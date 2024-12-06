0019920d19d94e715404d9a844a2756bd4371b0a
Tom Smilack <tom@smilack.net>
2024-12-06 12:05:57 -0600

    Reimplement sumMap more generically and add comments to most functions

--- src/Util.purs
+++ src/Util.purs
@@ -38,15 +38,17 @@ import Data.List.Types (NonEmptyList)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe, fromMaybe)
+import Data.Monoid.Additive (Additive(..))
+import Data.Newtype (alaF)
 import Data.String (split)
 import Data.String.CodeUnits (toCharArray)
 import Data.String.Pattern (Pattern(..))
 import Data.Symbol (class IsSymbol)
-import Data.Traversable (class Foldable, class Traversable, foldl)
+import Data.Traversable (class Foldable, class Traversable, foldMap)
 import Data.Tuple (Tuple(..), fst)
 import Effect.Aff (Error)
 import Parsing (Parser, runParser)
-import Parsing.Combinators (choice, many1, sepBy1, sepEndBy, try, (<?>), (<|>))
+import Parsing.Combinators (choice, sepBy1, sepEndBy, try, (<?>), (<|>))
 import Parsing.String (char, string)
 import Partial.Unsafe (unsafePartial)
 import PointFree ((<..))
@@ -55,15 +57,20 @@ import Record as Rec
 import Test.Spec.Assertions (shouldEqual)
 import Type.Proxy (Proxy(..))
 
-sumMap :: forall a f. Functor f => Foldable f => (a -> Int) -> f a -> Int
-sumMap = foldl add 0 <.. map
+-- map a function over a functor and add all results
+sumMap
+  :: forall a b f. Functor f => Foldable f => Semiring b => (a -> b) -> f a -> b
+sumMap = alaF Additive foldMap
 
+-- convert a string to a matrix of chars
 to2dArray :: String -> Array (Array Char)
 to2dArray = map toCharArray <<< lines
 
+-- split a string on newlines into an Array
 lines :: String -> Array String
 lines = split (Pattern "\n")
 
+-- parse any string and discard result
 skip :: String -> Parser String Unit
 skip = string >=> const (pure unit)
 
@@ -72,11 +79,13 @@ skip = string >=> const (pure unit)
 -- `between' 10 1 x` is like `x in [10]`
 -- `between' 10 2 x` is like `x in [10, 11]`
 -- `between' 10 5 x` is like `x in [10, 11, 12, 13, 14]`
+-- could also be described as `x ∈ [start, start + length)`
 between' :: forall a. Ord a => Ring a => a -> a -> a -> Boolean
 between' start length
   | length <= zero = const false
   | otherwise = between start (start + length - one)
 
+-- Non-empty range: always returns at least [start] even if length < 0
 range' :: forall a. Ord a => Ring a => a -> a -> NonEmptyArray a
 range' start length = go (singleton start) (start + one) (length - one)
   where
@@ -85,6 +94,9 @@ range' start length = go (singleton start) (start + one) (length - one)
     | otherwise = go (snoc a n) (n + one) (l - one)
 
 -- heron's formula for sqrt of any euclidean ring
+-- euclidian ring: ring plus division
+-- ring: semiring plus subtraction
+-- semiring: addition and multiplication
 hSqrt :: forall a. Eq a => EuclideanRing a => a -> a
 hSqrt n = go one zero
   where
@@ -98,6 +110,8 @@ inc = add one
 dec :: forall a. Ring a => a -> a
 dec = (_ - one)
 
+-- create new record by applying a function (a -> b) on @"key" of record
+-- shorthand for `Record.modify (Proxy :: Proxy "key") f r`
 modify
   :: forall @l r1 r2 r a b
    . IsSymbol l
@@ -108,6 +122,8 @@ modify
   -> Record r2
 modify = Rec.modify (Proxy :: _ l)
 
+-- `shouldEqual (runParser "1 2" spaced) (Right (1 : 2 : Nil))` ===
+-- `testParser "1 2" (1 : 2 : Nil) spaced`
 testParser
   :: forall m a
    . MonadThrow Error m
@@ -120,6 +136,11 @@ testParser
 testParser input expected parser =
   runParser input parser `shouldEqual` Right expected
 
+-- I think it's like
+-- `oneOfChar [('a' /\ 1), ('b' /\ 2)]` parses a single character of the string
+--   and maps it using the tuple?
+-- but it's not used anywhere
+-- maybe it's here because it's an easy copy of oneOfString?
 oneOfChar
   :: forall a t
    . Traversable t
@@ -128,6 +149,8 @@ oneOfChar
   -> Parser String a
 oneOfChar = oneOf' char
 
+-- I think it parses & maps a string based on the mapping in the tuples
+-- might just exist as a helper for genericParser
 oneOfString
   :: forall a t
    . Traversable t
@@ -136,6 +159,9 @@ oneOfString
   -> Parser String a
 oneOfString = oneOf' string
 
+-- take a list of tuples of type `Input /\ Output`
+-- create a parser for each of them that parses `fst` and swaps it for `snd`
+-- parse one of them, or fail and show a list of valid values
 oneOf'
   :: forall a1 a2 s t
    . Traversable t
@@ -150,6 +176,8 @@ oneOf' p xs = choice parsers <?> expected
   cs = map fst xs
   expected = "one of " <> show cs
 
+-- automatically generate a parser for a generic type
+-- e.g. parse "A" as a value of a type `A | B | C`
 genericParser :: forall @a. BoundedEnum a => Show a => Parser String a
 genericParser = oneOfString $ map (\a -> Tuple (show a) a) as
   where
@@ -180,6 +208,7 @@ tally = Map.fromFoldableWith (+) <<< flip Lazy.zip ones <<< Lazy.fromFoldable
   where
   ones = Lazy.repeat 1
 
+-- with `Monoid v` constraint, equivalent to `fold <.. lookup`
 lookupWithDefault :: forall k v. Ord k => v -> k -> Map k v -> v
 lookupWithDefault v = fromMaybe v <.. Map.lookup
 
