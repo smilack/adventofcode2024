9c634ffec239c3fa00b8a958e2e381868e13979b
Tom Smilack <tom@smilack.net>
2024-12-19 01:12:12 -0600

    Tons of messing around with RowLists/type level programming/trying to traverse records

--- src/Six.purs
+++ src/Six.purs
@@ -10,6 +10,11 @@ import AdventOfCode.Twenty24.Util (multiline, oneOfChar)
 import AdventOfCode.Util.Area (Area)
 import AdventOfCode.Util.Area as Area
 import AdventOfCode.Util.Coord (Coord(..), Direction(..))
+import Data.Array as Array
+import Data.FoldableWithIndex (findWithIndex)
+import Data.Reflectable (class Reflectable, class Reifiable, reflectType, reifyType)
+import Data.Symbol (class IsSymbol)
+import Debug (spy, traceM)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
@@ -17,6 +22,12 @@ import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
 import Parsing (Parser)
+import Record (get, insert, merge, modify, set, union, delete)
+import Type.Proxy (Proxy(..))
+import Type.Row (type (+), class Lacks, class Cons, class Union)
+import Type.RowList as RL
+
+-- import Type.RowList (Cons, Nil, class ListToRow)
 
 main :: Effect Unit
 main = launchAff_ do
@@ -27,9 +38,74 @@ main = launchAff_ do
     log "Part 2:"
     -- logShow $ solve2 input
     log "End"
+    --
+    log "\nhi"
+    log $ show @(ShowKeysOnly State StateList) $ ShowKeysOnly
+      { pos: false, lab: 0, dir: "a" }
 
--- solve1 :: String -> List (List LabCell)
--- solve1 = parseLab
+--
+newtype ShowKeysOnly :: Row Type -> RL.RowList Type -> Type
+newtype ShowKeysOnly r l = ShowKeysOnly (Record r)
+
+class ShowKeysInRowList2 :: Row Type -> RL.RowList Type -> Constraint
+class RL.RowToList r l <= ShowKeysInRowList2 r l | l -> r where
+  buildKeyList2 :: Record r -> List String
+
+instance ShowKeysInRowList2 () RL.Nil where
+  buildKeyList2 _ = Nil
+
+instance
+  ( Reflectable sym String
+  , ShowKeysInRowList2 r' rest
+  , IsSymbol sym
+  , Cons sym k r' r
+  , RL.RowToList r (RL.Cons sym k rest)
+  , Lacks sym r'
+  ) =>
+  ShowKeysInRowList2 r (RL.Cons sym k rest)
+  where
+  buildKeyList2 :: Record r -> List String
+  buildKeyList2 rec = (Cons :: String -> List String -> List String) thisKey
+    remainingKeys
+    where
+    thisKey = reflectType (Proxy @sym)
+
+    _ = spy "get thisKey rec" $ get (Proxy @sym) rec
+
+    restRec = delete (Proxy @sym) rec
+
+    remainingKeys = buildKeyList2 @r' @rest restRec
+
+instance
+  ( ShowKeysInRowList2 recordRows rowList
+  ) =>
+  Show (ShowKeysOnly recordRows rowList)
+  where
+  show (ShowKeysOnly rec) = show $ buildKeyList2 @recordRows @rowList rec
+
+-- type State =
+--   { pos :: Coord
+--   , lab :: Area LabCell
+--   , dir :: Direction
+--   }
+type State =
+  ( dir :: String
+  , lab :: Int
+  , pos :: Boolean
+  )
+
+type StateList =
+  (RL.Cons "dir" String (RL.Cons "lab" Int (RL.Cons "pos" Boolean RL.Nil)))
+
+addrec
+  :: forall l a r1 r2
+   . IsSymbol l
+  => Lacks l r1
+  => Cons l a r1 r2
+  => Tuple (Proxy l) a
+  -> Record r1
+  -> Record r2
+addrec (l /\ a) = insert l a
 
 turn :: Direction -> Direction
 turn = case _ of
---
+++ src/Util/SeqRec.purs
@@ -0,0 +1,50 @@
+module AdventOfCode.Twenty24.Util.SeqRec where
+
+import AdventOfCode.Prelude
+import Data.Symbol (class IsSymbol)
+import Record (get, set, insert)
+import Type.Proxy (Proxy(..))
+import Type.Row (class Lacks, class Cons)
+
+seqrec
+  :: forall @l a r0 r1
+   . IsSymbol l
+  => Cons l (Maybe a) () r0
+  => Cons l a () r1
+  => Record r0
+  -> Maybe (Record r1)
+seqrec r0 = case get l r0 of
+  Nothing -> Nothing
+  Just val -> Just $ set l val r0
+  where
+  l = Proxy @l
+
+seqadd
+  :: forall @l a r0 r1 r2
+   . IsSymbol l
+  => Cons l (Maybe a) () r0
+  => Lacks l r1
+  => Cons l a r1 r2
+  => Record r0
+  -> Maybe (Record r1)
+  -> Maybe (Record r2)
+seqadd = flip (addseq @l)
+
+addseq
+  :: forall @l a r0 r1 r2
+   . IsSymbol l
+  => Cons l (Maybe a) () r0
+  => Lacks l r1
+  => Cons l a r1 r2
+  => Maybe (Record r1)
+  -> Record r0
+  -> Maybe (Record r2)
+addseq mr1 r0 = case mr1 of
+  Nothing -> Nothing
+  Just r1 -> case get l r0 of
+    Nothing -> Nothing
+    Just a -> Just (insert l a r1)
+  where
+  l = Proxy @l
+
+infixl 5 addseq as <>?
