4365640950afa3f371dcadb6a0ba31975f6735ac
Tom Smilack <tom@smilack.net>
2024-12-20 01:41:48 -0600

    Partial solution for day 6 part 2 that doesn't catch long cycles

--- src/Six.purs
+++ src/Six.purs
@@ -1,9 +1,12 @@
 module AdventOfCode.Twenty24.Six
   ( LabCell(..)
   , initialState
+  , isRect
   , main
+  , possibleCoords
   , run
   , solve1
+  , solve2
   , toMap
   ) where
 
@@ -14,16 +17,19 @@ import AdventOfCode.Twenty24.Util.SeqRec (seqrec, (<>?))
 import AdventOfCode.Util.Area (Area)
 import AdventOfCode.Util.Area (get, set) as A
 import AdventOfCode.Util.Area as Area
-import AdventOfCode.Util.Coord (Coord(..), Direction(..), move)
+import AdventOfCode.Util.Coord (Coord(..), Direction(..), move, dist)
 import Data.Array as Array
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..)) as E
 import Data.FoldableWithIndex (findWithIndex)
+import Data.List (length, take)
 import Data.Monoid.Additive (Additive(..))
 import Data.Newtype (alaF)
 import Data.Reflectable (class Reflectable, class Reifiable, reflectType, reifyType)
 import Data.Symbol (class IsSymbol)
-import Debug (spy, traceM)
+import Data.Typelevel.Num (d0, d1, d2, d3)
+import Data.Vec (Vec, empty, (+>), (!!), sort)
+import Debug (spy, spyWith, traceM)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
@@ -31,6 +37,7 @@ import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
 import Parsing (Parser)
+import Record (insert)
 import Type.Proxy (Proxy(..))
 import Type.Row (type (+), class Lacks, class Cons, class Union)
 import Type.RowList as RL
@@ -46,6 +53,77 @@ main = launchAff_ do
     log "Part 2:"
     -- logShow $ solve2 input
     log "End"
+
+-- part 2
+-- 1. starting from right side of an obstacle (r, c)
+-- 2. go north until hitting an obstacle (r', c)
+-- 3. go east until hitting an obstacle (r', c')
+-- 4. go south until first of:
+--    A. hitting an obstacle -> stop
+--    B. (r, c') -> continue
+-- 5. go west until first of
+--    A. obscacle -> stop
+--    B. (r, c) -> (r, c') is valid
+
+solve2 :: String -> Int
+solve2 = length <<< possibleCoords
+
+possibleCoords :: String -> List Coord
+possibleCoords = fold
+  <<< map run2
+  <<< initialState2
+  <<< toMap
+
+isRect :: Coord -> Coord -> Coord -> Coord -> Boolean
+isRect a b c d =
+  near top _.y (-1)
+    && near right _.x 1
+    && near left _.x 1
+    && near bottom _.y (-1)
+  where
+  points = sort $ a +> b +> c +> d +> empty
+  --
+  top = spyWith "top" show $ (points !! d0) /\ (points !! d1)
+  right = spyWith "right" show $ (points !! d1) /\ (points !! d3)
+  bottom = spyWith "bottom" show $ (points !! d2) /\ (points !! d3)
+  left = spyWith "left" show $ (points !! d0) /\ (points !! d2)
+  --
+  near pts f n = (f $ uncurry dist pts) == n
+
+type State2 = Record (St (bumps :: List Coord, poss :: List Coord))
+
+initialState2 :: Area LabCell -> Maybe State2
+initialState2 =
+  map
+    (insert (Proxy @"poss") Nil <<< insert (Proxy @"bumps") Nil)
+    <<< initialState
+
+run2 :: State2 -> List Coord
+run2 = go <<< E.Right
+  where
+  go = case _ of
+    E.Left coords -> coords
+    E.Right state@{ lab, pos, dir, bumps, poss } -> go $
+      let
+        peek = move dir pos
+      in
+        case A.get peek (spyWith "map" (("\n" <> _) <<< show) lab) of
+          Just Obstructed -> E.Right $ state
+            { dir = turn dir
+            , bumps = peek : bumps
+            , lab = A.set pos (Guard $ turn dir) lab
+            }
+          Just Empty -> E.Right $ nextMove $ checkPoss peek state
+          Just Visited -> E.Right $ nextMove $ checkPoss peek state
+          _ -> E.Left poss
+
+  checkPoss :: Coord -> State2 -> State2
+  checkPoss peek state = case state.bumps of
+    (a : b : c : _) ->
+      if isRect a b c peek then state
+        { poss = spyWith "adding" show peek : state.poss }
+      else state
+    _ -> state
 
 solve1 :: String -> Int
 solve1 = unwrap
@@ -67,10 +145,11 @@ run = go <<< E.Right
         Just Visited -> E.Right $ nextMove state
         _ -> E.Left $ A.set pos Visited lab
 
-  nextMove state@{ lab, pos, dir } = state
-    { lab = A.set pos Visited $ A.set (move dir pos) (Guard dir) lab
-    , pos = move dir pos
-    }
+nextMove :: forall r. Record (St r) -> Record (St r)
+nextMove state@{ lab, pos, dir } = state
+  { lab = A.set pos Visited $ A.set (move dir pos) (Guard dir) lab
+  , pos = move dir pos
+  }
 
 initialState :: Area LabCell -> Maybe State
 initialState lab = seqrec { lab: Just lab } <>? { pos } <>? { dir }
@@ -94,11 +173,14 @@ directionOf = case _ of
   Guard d -> Just d
   _ -> Nothing
 
-type State =
-  { pos :: Coord
+type St r =
+  ( pos :: Coord
   , lab :: Area LabCell
   , dir :: Direction
-  }
+  | r
+  )
+
+type State = Record (St ())
 
 turn :: Direction -> Direction
 turn = case _ of
--- test/Six.purs
+++ test/Six.purs
@@ -4,7 +4,11 @@ module Test.AdventOfCode.Twenty24.Six
 
 import AdventOfCode.Prelude
 
-import AdventOfCode.Twenty24.Six (initialState, run, solve1, toMap)
+import AdventOfCode.Twenty24.Six (possibleCoords, LabCell(..), initialState, isRect, run, solve1, solve2, toMap)
+import AdventOfCode.Util.Area (set) as A
+import AdventOfCode.Util.Coord (mkCoordRC)
+import Data.FoldableWithIndex (foldMapWithIndex)
+import Data.List (singleton)
 import Effect (Effect)
 import Effect.Class.Console (logShow)
 import Test.QuickCheck ((===), Result)
@@ -16,18 +20,50 @@ import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
 main :: Effect Unit
 main = do
-  logShow $ toMap testInput
+  let
+    testMap = toMap testInput
+    part2ans = possibleCoords testInput
+    part2poss = foldr (\coord -> A.set coord Visited) testMap part2ans
+  logShow $ testMap
+  logShow
+    $ foldMapWithIndex
+        ( \i -> case _ of
+            Obstructed -> singleton i
+            _ -> Nil
+        )
+    $ testMap
+  log testOut
+  logShow $ possibleCoords testInput
+  logShow $ part2poss
+  --
   runSpecAndExitProcess [ consoleReporter ] do
     describe "Day Six" do
       describe "Part 1" do
-        it "solve 1" do
-          solve1 testInput `shouldEqual` 41
+        it "solve 1" $ solve1 testInput `shouldEqual` 41
       describe "Part 2" do
-        pending "more stuff"
+        describe "isRect" do
+          it "n" $ isRect r0c4 r1c9 r3c2 r4c7 `shouldEqual` false
+          it "n" $ isRect r6c1 r7c8 r8c0 r9c6 `shouldEqual` false
+          it "y" $ isRect r6c1 r7c8 r8c0 (mkCoordRC 9 7) `shouldEqual` true
+          it "n" $ isRect r0c4 r3c2 r6c1 r8c0 `shouldEqual` false
+        it "solve 2" $ solve2 testInput `shouldEqual` 6
 
 testInput :: String
 testInput =
   "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#..."
+
+testParsed :: String
+testParsed =
+  """░░░░█░░░░░
+░░░░░░░░░█
+░░░░░░░░░░
+░░█░░░░░░░
+░░░░░░░█░░
+░░░░░░░░░░
+░█░░↑░░░░░
+░░░░░░░░█░
+█░░░░░░░░░
+░░░░░░█░░░"""
 
 testOut :: String
 testOut =
@@ -41,3 +77,12 @@ testOut =
 ░       █░
 █       ░░
 ░░░░░░█ ░░"""
+
+r0c4 = mkCoordRC 0 4
+r1c9 = mkCoordRC 1 9
+r3c2 = mkCoordRC 3 2
+r4c7 = mkCoordRC 4 7
+r6c1 = mkCoordRC 6 1
+r7c8 = mkCoordRC 7 8
+r8c0 = mkCoordRC 8 0
+r9c6 = mkCoordRC 9 6
