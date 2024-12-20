94f7ab22961a976e8207983f5f11a71d56b26654
Tom Smilack <tom@smilack.net>
2024-12-19 20:20:58 -0600

    Migrate a bunch of type-level record testing stuff from Six

--- src/Util/SeqRec.purs
+++ src/Util/SeqRec.purs
@@ -7,6 +7,72 @@ import Type.Proxy (Proxy(..))
 import Type.Row (class Lacks, class Cons)
 import Type.RowList (Cons, Nil, class RowToList)
 
+main :: Effect Unit
+main = do
+  log "SeqRec testing"
+  --
+  log "End"
+
+-- ┌──────────────┐
+-- │ SeqRec class │
+-- └──────────────┘
+
+newtype ShowKeysOnly :: Row Type -> RL.RowList Type -> Type
+newtype ShowKeysOnly r l = ShowKeysOnly (Record r)
+
+class ShowKeysInRowList2 :: Row Type -> RL.RowList Type -> Constraint
+class RL.RowToList r l <= ShowKeysInRowList2 r l | l -> r where
+  buildKeyList2 :: Record r -> List String
+
+instance ShowKeysInRowList2 () RL.Nil where
+  buildKeyList2 _ = Nil
+
+instance
+  ( Reflectable sym String
+  , ShowKeysInRowList2 r' rest
+  , IsSymbol sym
+  , Cons sym k r' r
+  , RL.RowToList r (RL.Cons sym k rest)
+  , Lacks sym r'
+  ) =>
+  ShowKeysInRowList2 r (RL.Cons sym k rest)
+  where
+  buildKeyList2 :: Record r -> List String
+  buildKeyList2 rec = (Cons :: String -> List String -> List String) thisKey
+    remainingKeys
+    where
+    thisKey = reflectType (Proxy @sym)
+
+    _ = spy "get thisKey rec" $ get (Proxy @sym) rec
+
+    restRec = delete (Proxy @sym) rec
+
+    remainingKeys = buildKeyList2 @r' @rest restRec
+
+instance
+  ( ShowKeysInRowList2 recordRows rowList
+  ) =>
+  Show (ShowKeysOnly recordRows rowList)
+  where
+  show (ShowKeysOnly rec) = show $ buildKeyList2 @recordRows @rowList rec
+
+type State =
+  ( dir :: String
+  , lab :: Int
+  , pos :: Boolean
+  )
+
+type StateList =
+  (RL.Cons "dir" String (RL.Cons "lab" Int (RL.Cons "pos" Boolean RL.Nil)))
+
+-- ┌─────────────────────────────────────────────────────────────────┐
+-- │ seqrec, addseq, <>?                                             │
+-- ├─────────────────────────────────────────────────────────────────┤
+-- │ seqrec { a: Just 1 } <>? { b: Just "yes" } <>? { c: Just true } │
+-- ├─────────────────────────────────────────────────────────────────┤
+-- │   == Just { a: 1, b: "yes", c: true }                           │
+-- └─────────────────────────────────────────────────────────────────┘
+
 seqrec
   :: forall k v r0 r1
    . IsSymbol k
