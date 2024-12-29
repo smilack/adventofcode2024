852141de6ffdb12421363cc5bd3e2953fa1a077e
Tom Smilack <tom@smilack.net>
2024-12-29 09:31:24 -0600

    Explicitly export seqrec and addseq. Remove unused seqadd.

--- src/Util/SeqRec.purs
+++ src/Util/SeqRec.purs
@@ -1,4 +1,4 @@
-module AdventOfCode.Twenty24.Util.SeqRec where
+module AdventOfCode.Twenty24.Util.SeqRec (seqrec, addseq, (<>?)) where
 
 import AdventOfCode.Prelude hiding (sequence)
 
@@ -120,18 +120,6 @@ seqrec
 seqrec r0 = (\val -> set key val r0) <$> get key r0
   where
   key = Proxy @k
-
-seqadd
-  :: forall k v r0 r1 r2
-   . IsSymbol k
-  => Cons k (Maybe v) () r0
-  => RowToList r0 (Cons k (Maybe v) Nil)
-  => Lacks k r1
-  => Cons k v r1 r2
-  => Record r0
-  -> Maybe (Record r1)
-  -> Maybe (Record r2)
-seqadd = flip addseq
 
 addseq
   :: forall k v r0 r1 r2
