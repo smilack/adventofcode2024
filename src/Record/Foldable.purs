a17aed84cf0e1d6a893fb198944b7f1f129f46a2
Tom Smilack <tom@smilack.net>
2025-01-03 15:45:13 -0600

    Working on adding functions to helper classes to propagate row/rowlist equality

--- src/Record/Foldable.purs
+++ src/Record/Foldable.purs
@@ -2,6 +2,7 @@ module Record.Foldable where
 
 import AdventOfCode.Prelude hiding (sequence)
 
+import Data.Newtype (class Newtype)
 import Data.Symbol (class IsSymbol)
 import Debug (traceM)
 import Prim.RowList (RowList)
@@ -12,22 +13,22 @@ import Type.Row (class Lacks, class Cons)
 import Type.RowList (class ListToRow, class RowToList, Cons, Nil)
 
 -- | Proxy type to hold RowLists
-data RowListProxy :: RowList Type -> Type
-data RowListProxy rlpT = RowListProxy
+-- data R_owListProxy :: RowList Type -> Type
+-- data R_owListProxy rlpT = R_owListProxy
 
 -- | For some Applicative `ap`, ensure that every value in a Record (specified
 -- | as a proxy with a RowList Type parameter) has type `ap a`.
 class RecordOfAp :: (Type -> Type) -> Type -> Constraint
 class RecordOfAp ap rlApV
 
-instance recordOfApNil :: (Applicative f) => RecordOfAp f (RowListProxy Nil)
+instance recordOfApNil :: (Applicative f) => RecordOfAp f (Proxy Nil)
 
 instance recordOfApCons ::
   ( Applicative ap
   , IsSymbol k
-  , RecordOfAp ap (RowListProxy rlApT)
+  , RecordOfAp ap (Proxy rlApT)
   ) =>
-  RecordOfAp ap (RowListProxy (Cons k (ap v) rlApT))
+  RecordOfAp ap (Proxy (Cons k (ap v) rlApT))
 
 -- | For some Applicative `ap`, ensure that two Records (specified as a proxies
 -- | with RowList Type parameters) have the same keys and that for each value
@@ -39,38 +40,66 @@ class UnApRecord ap rlpApV rlpUnV | ap rlpUnV -> rlpApV, ap rlpApV -> rlpUnV
 instance unApRecordNil ::
   ( Applicative f
   ) =>
-  UnApRecord ap (RowListProxy Nil) (RowListProxy Nil)
+  UnApRecord ap (Proxy Nil) (Proxy Nil)
 
 instance unApRecordCons ::
   ( Applicative ap
   , IsSymbol k
-  , UnApRecord ap (RowListProxy rlpApT) (RowListProxy rlpUnT)
+  , UnApRecord ap (Proxy rlpApT) (Proxy rlpUnT)
   ) =>
   UnApRecord ap
-    (RowListProxy (Cons k (ap a) rlpApT))
-    (RowListProxy (Cons k a rlpUnT))
+    (Proxy (Cons k (ap a) rlpApT))
+    (Proxy (Cons k a rlpUnT))
 
--- | Unwrap a `RowListProxy rlpT` and convert `rlpT` to a Row Type.
-class ListProxyToRow :: Type -> Row Type -> Constraint
-class ListProxyToRow rlpV row | row -> rlpV, rlpV -> row
+-- | Unwrap a `Proxy rlpT` and convert `rlpT` to a Row Type.
+class RowToListProxy :: Type -> RowList Type -> Row Type -> Constraint
+class RowToListProxy p rl row where
 
-instance (RowToList row rlpT) => ListProxyToRow (RowListProxy rlpT) row
+-- ltor
+--   :: forall
+--    . 
+--   => Proxy 
+--   -> row
+--   -> row
+
+instance (RowToList row rl) => RowToListProxy (Proxy rl) rl row
 
 -- | Wrapper to allow writing instances for records.
 newtype SeqRec :: Type -> Type
 newtype SeqRec recAp = SeqRec recAp
 
+derive instance Newtype (SeqRec a) _
+{- 
 -- | Constraints allowing sequencing a record held by a newtype.
-class SequenceableRecord :: (Type -> Type) -> Type -> Type -> Constraint
-class SequenceableRecord t rlpApV rowNoAp where
+class SequenceableRecord :: (Type -> Type) -> RowList Type -> Constraint
+class SequenceableRecord t rlAp where
   sequenceImpl
-    :: forall ap rowAp rowUn rlpUnT
+    :: forall ap rowAp rowUn rlUn
      . Applicative ap
-    => ListProxyToRow rlpApV rowAp
-    => RecordOfAp ap rlpApV
-    => UnApRecord ap rlpApV (RowListProxy rlpUnT)
-    => ListToRow rlpUnT rowUn
-    => rlpApV
+    => ListProxyToRow (Proxy rlAp) rowAp
+    => Newtype (t (Record rowAp)) (Record rowAp)
+    => RecordOfAp ap (Proxy rlAp)
+    => UnApRecord ap (Proxy rlAp) (Proxy rlUn)
+    => ListToRow rlUn rowUn
+    => Proxy rlAp
     -> t (Record rowAp)
     -> ap (Record rowUn)
 
+instance sequenceableRecordNil :: SequenceableRecord t Nil where
+  sequenceImpl
+    :: forall ap rowAp rowUn
+     . Applicative ap
+    => ListProxyToRow (Proxy Nil) rowAp
+    => Newtype (t (Record rowAp)) (Record rowAp)
+    => RecordOfAp ap (Proxy Nil)
+    => UnApRecord ap (Proxy Nil) (Proxy Nil)
+    => ListToRow Nil rowUn
+    => Proxy Nil
+    -> t (Record rowAp)
+    -> ap (Record rowUn)
+  sequenceImpl _ t = pure recUn
+    where
+    recAp = (unwrap t) :: Record rowAp
+
+
+    recUn = () :: Record rowUn -}
